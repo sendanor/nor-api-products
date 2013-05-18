5d18956bf787c0fc436eda2ad4621eb5beca713e
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-05-18 05:48:09 +0300

    Fixed typo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 /* Setup our API */
 
 var config = require('nor-config').from(__dirname);
-config._def('docroot', require('path').join(__dirname, '..', 'docroot');
+config._def('docroot', require('path').join(__dirname, '..', 'docroot'));
 require('nor-api').createServer(config, {
 	'docroot': config.docroot,
 	'routes': {
